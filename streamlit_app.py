0e10ee46b6e26c4eb08af6fd06eb4ed3994eb3e5
FluffySnowflake <110967839+FluffySnowflake@users.noreply.github.com>
2022-08-11 22:17:56 +0100

    Update streamlit_app.py
    converting the response with json function

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,9 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 
-
+# New section to display fruityvice fruit advice
 import requests
+
+streamlit.header('Fruityvice Fruit Advice')
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+streamlit.text(fruityvice_response.json())
